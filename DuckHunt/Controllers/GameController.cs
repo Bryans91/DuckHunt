a9cef01a986cb4a2af8001618bbc6fdb161aa267
Bryan Schreuder <bryanschreuder91@gmail.com>
2015-09-06 15:46:27 +0200

    eend

---
+++ DuckHunt/Controllers/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace DuckHunt.Controllers
+{
+    class GameController
+    {
+
+        private ArrayList ducks;
+        private MainWindow n = new MainWindow();
+
+        public GameController()
+        {
+            ducks = new ArrayList();
+            //create duck and add to this.ducks
+           
+            var duckie = n.DrawRect(25, 25, 200, 200);
+            ducks.Add(duckie);
+            Thread t = new Thread(new ThreadStart(gameLoop));
+            t.Start();
+        }
+
+        public void gameLoop()
+        {
+          while(ducks.Count > 0)
+            {
+                foreach(Rectangle d in ducks)
+                {
+                    n.moveRect(d,2);
+                }
+               
+                //moveducks
+            }
+
+        }
+
+
+    }
+}
---
+++ DuckHunt/Views/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace DuckHunt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Rectangle DrawRect(int x, int y,int w,int h)
+        {
+            Rectangle rect = new Rectangle
+            {
+                Stroke = Brushes.LightBlue,
+                StrokeThickness = 2,
+                Width = w,
+                Height = h
+            
+            };
+            Canvas.SetLeft(rect, x);
+            Canvas.SetTop(rect, y);
+            canvas.Children.Add(rect);
+            return rect;
+        }
+
+
+        public void moveRect(Rectangle rect, int velocity)
+        {
+            double pos = Canvas.GetLeft(rect);
+            Canvas.SetLeft(rect, pos+velocity);
+           
+        }
+       
+    }
+}
